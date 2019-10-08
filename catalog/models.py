b5e950494ae2164b5f1db329907010dfb9640df2
RodolfoNei <rodolfonei.webdev@gmail.com>
2019-10-07 21:40:48 -0300

    criando o model do Gênero de livros

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    """Model representando um gênero de livro"""
+    name = models.CharField(max_length=200, help_text='Insira um gênero de livro (e.g. Ficção Científica)')
+
+    def __str__(self):
+        """String para representar o objeto do Model"""
+        return self.name
