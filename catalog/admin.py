b1e3d13ba18ef6388064a3c393a9250247f4319a
RodolfoNei <rodolfonei.webdev@gmail.com>
2019-10-09 16:34:56 -0300

    registrando os novos models no admin do site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from catalog.models import Author, Genre, Book
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
