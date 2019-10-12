e9a248648fb3ad7cc78f06f9f79ccdad2658377c
RodolfoNei <rodolfonei.webdev@gmail.com>
2019-10-12 08:30:23 -0300

    personalizando o display dos models na página admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from catalog.models import Author, Genre, Book
 
-admin.site.register(Author)
+# admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'year', 'status')
+
 admin.site.register(Genre)
-admin.site.register(Book)
+
