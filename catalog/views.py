f5ef1f01a252aee3a705d7e3ab71de52928680f6
RodolfoNei <rodolfonei.webdev@gmail.com>
2019-10-22 18:42:51 -0300

    definindo a função para o views do index

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, Genre
 
-# Create your views here.
+def index(request):
+    """View function para a home page do site."""
+
+    # Gerar a contagem do número de livros cadastrados
+    num_books = Book.objects.all().count()
+
+    # Gerar a contagem de livros segundo cada status
+    num_want = Book.objects.filter(status__exact='w').count()
+    num_already = Book.objects.filter(status__exact='a').count()
+    num_now = Book.objects.filter(status__exact='r').count()
+
+    # Gerar a contagem de autores cadastrados
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_want': num_want,
+        'num_already': num_already,
+        'num_now': num_now,
+        'num_authors': num_authors,
+    }
+
+    # Renderizar o template HTML index.html com os dados da var context
+    return render(request, 'index.html', context=context)
+
