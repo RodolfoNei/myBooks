75cc364c7c0d34b5e1173f5e76f1a7e6714017e0
RodolfoNei <rodolfonei.webdev@gmail.com>
2020-06-06 00:23:21 -0300

    criação da página Author List

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('book/<str:pk>', views.BookDetailView.as_view(), name='book-detail')
+    path('book/<str:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -32,3 +32,6 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
